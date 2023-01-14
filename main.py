41f24b34fb439cb698fc544ccb5879143474001d
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2023-01-14 16:28:05 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 
+import time
+
+screen = Screen()
+screen.setup(width=1200, height=800)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = ScoreBoard()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
