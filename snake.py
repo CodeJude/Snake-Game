3c9d65f6c7948449701bab700593af616fe44fc1
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2023-01-18 00:57:59 +0400

    Update snake.py

--- snake.py
+++ snake.py
@@ -9,30 +9,7 @@ RIGHT = 0
 
 class Snake:
 
-    def __init__(self):
-        self.segments = []
-        self.create_snake()
-        self.head = self.segments[0]
-
-    def create_snake(self):
-        for turtle_index in STARTING_POSITIONS:
-            new_turtle = Turtle(shape="square")
-            new_turtle.color("white")
-            new_turtle.penup()
-            new_turtle.goto(turtle_index)
-            self.segments.append(new_turtle)
-
-    def move(self):
-        for seg_num in range(len(self.segments) - 1, 0, -1):
-            new_x = self.segments[seg_num - 1].xcor()
-            new_y = self.segments[seg_num - 1].ycor()
-            self.segments[seg_num].goto(new_x, new_y)
-
-        self.head.forward(MOVE_DISTANCE)
-
-    def up(self):
-        if self.head.heading() != DOWN:
-            self.head.setheading(UP)
+   
 
     def down(self):
         if self.head.heading() != UP:
